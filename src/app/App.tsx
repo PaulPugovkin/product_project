5869d457062ed6cf7465f9f44ce67b82ec633b50
Paul <63363585+PaulPugovkin@users.noreply.github.com>
2022-09-18 18:35:32 +0300

    refactor to FSD

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import {Suspense} from 'react'
 import {Link, Route, Routes} from 'react-router-dom'
 import './styles/index.scss'
-import {AboutPageAsync} from './pages/AboutPage/AboutPage.async'
-import {MainPageAsync} from './pages/MainPage/MainPage.async'
-import {useTheme} from './theme/useTheme'
-import {classNames} from './helpers/classNames/classNames'
+import {classNames} from "shared/lib/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {AboutPageAsync} from "pages/AboutPage";
+import {MainPageAsync} from "pages/MainPage";
 
 export const App = () => {
     const {theme, toggleTheme} = useTheme()
